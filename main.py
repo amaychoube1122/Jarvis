efa54aed3c2bfe0b8e36e15ac31aad48b5c2c881
Amaychoube1122 <amaychoube1122@gmail.com>
2025-01-28 23:49:48 +0530

    day2 complete

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import eel
+
+# from engine.features import *
+# from engine.command import *
+# from engine.auth import recoganize
+# def start():
+eel.init("www")
+
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+eel.start('index.html', mode=None, host='localhost', block=True)
+ 
